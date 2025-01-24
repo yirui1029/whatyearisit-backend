957d03ad9ab3a90fb3575bdab99fb4e31634a8cf
yirui Dupuy <yirui.hao@gmail.com>
2025-01-24 10:56:07 +0100

    whatyearisit

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var yearRouter= require('./routes/year');
 
 var app = express();
-var yearRouter = require('../whatyearisit-backend/route/year');
-
-
 const cors = require('cors');
 app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', yearRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/year',yearRouter);
 
 module.exports = app;
--- routes/year.js
+++ routes/year.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get('/year', (req, res) => {
+router.get('/', (req, res) => {
     const Year = new Date().getFullYear().toString();
     console.log(Year);
     res.json({ year: Year });
